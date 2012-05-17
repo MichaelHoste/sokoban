9dbc97ec9e6765bc4937c663683327e529cf03a1
Michael Hoste <michael.hoste@gmail.com>
2012-05-17 15:30:55 +0200

    configuration api facebook

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,6 +1,4 @@
-# You need to create a file "/config/initializers/facebook.rb" with this :
-# ENV['FACEBOOK_KEY'] = '11111111111111'
-# ENV['FACEBOOK_SECRET'] = '1X1X1X1X1X1X1#X1X1X1X1X1X1X1X1X1X1X1'
+# Rename the file "/config/initializers/facebook.rb.example" and adapt it
 
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET'],
