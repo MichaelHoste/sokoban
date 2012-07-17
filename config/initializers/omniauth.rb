02b3fac51721793fa69c9b711d5725b879af4461
Michael Hoste <michael.hoste@gmail.com>
2012-07-17 11:15:05 +0200

    debut de l'intégration facebook open graph

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,5 +2,5 @@
 
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET'],
-           :scope => 'email'
+           :scope => 'email, publish_actions'
 end
