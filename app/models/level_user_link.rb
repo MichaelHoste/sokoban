6f62279d669d00138b1497bf030a244e1c68e42b
Michael Hoste <michael.hoste@gmail.com>
2012-07-17 12:12:10 +0200

    debug

--- app/models/level_user_link.rb
+++ app/models/level_user_link.rb
@@ -67,6 +67,7 @@ class LevelUserLink < ActiveRecord::Base
   # publish the "user has completed the level" on open graph (facebook)
   def publish_on_facebook
     graph = Koala::Facebook::API.new(user.f_token)
+    Rails.logger.info("ICI : " + "http://sokoban.be/packs/#{self.level.pack.name}/levels/#{self.level.name}")
     graph.put_connections("me", "sokojax:complete", :level  => "http://sokoban.be/packs/#{self.level.pack.name}/levels/#{self.level.name}",
                                                     :pushes => self.pushes,
                                                     :moves  => self.moves)
