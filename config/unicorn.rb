f73f5e4bfc95b97ad389bae73abad607104ff147
Michael Hoste <michael.hoste@gmail.com>
2012-08-01 14:03:10 +0200

    ajout des fichiers de config nginx et unicorn

---
+++ config/unicorn.rb
@@ -0,0 +1,8 @@
+working_directory "/home/deploy/apps/sokoban/current"
+pid "/home/deploy/apps/sokoban/shared/pids/unicorn.pid"
+stderr_path "/home/deploy/apps/sokoban/shared/log/unicorn.log"
+stdout_path "/home/deploy/apps/sokoban/shared/log/unicorn.log"
+
+listen "/tmp/unicorn.sokoban.sock"
+worker_processes 2
+timeout 30
