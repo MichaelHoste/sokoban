a0a928e52be94a416e17ce033b4ef198f75c5d06
Michael Hoste <michael.hoste@gmail.com>
2012-11-10 09:38:34 +0100

    schedule

--- config/schedule.rb
+++ config/schedule.rb
@@ -2,10 +2,6 @@
 set      :output,      "#{path}/log/cron.log"
 job_type :bundle_exec, 'cd :path && bundle exec :task :output' # :path is set to project dir by default  
 
-every 5.minutes do
-  bundle_exec "backup perform --trigger Sokoban -c #{path}/config/backup.rb"
-end
-
 every 1.day, :at => '4:30 am' do
   bundle_exec "backup perform --trigger Sokoban -c #{path}/config/backup.rb"
 end
