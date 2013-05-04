5507e1a0e8a22d7686a4b676ba03d97eab8e222a
Michaël Hoste <michael.hoste@gmail.com>
2013-05-04 14:18:01 +0200

    amélioration de l'envoi du mail

--- app/services/mailing_service.rb
+++ app/services/mailing_service.rb
@@ -2,10 +2,22 @@ module MailingService
   # Automatic mailing (every 7 days unless the user was active less than 48hours ago)
   def self.delayed_send_mailing
     Delayed::Job.where(:queue => 'send_mailing').destroy_all
-    MailingService.delay(:run_at => Time.now + 10.minutes, :queue => 'send_mailing').send_mailing(true)
+    MailingService.delay(:run_at => Time.now + 30.minutes, :queue => 'send_mailing').send_mailing(true)
   end
 
   def self.send_mailing(repeat = false)
+    MailingService.users_to_mail_now.each do |user|
+      UserNotifier.delay.weekly_notification(user.id)
+      UserNotifier.delay.weekly_notification(user.id, true)
+      user.update_attributes!({ :next_mailing_at => Time.now + 7.days + rand(-12..12).hours })
+    end
+
+    if repeat
+      MailingService.delayed_send_mailing
+    end
+  end
+
+  def self.users_to_mail_now
     mail_users = User.registered.where(:mailing_unsubscribe => false)
                                 .where('next_mailing_at < ?', Time.now)
 
@@ -25,21 +37,12 @@ module MailingService
       end
     end
 
-    mail_users.each do |user|
-      # Delay mailing if user was connected less than 2 days ago
-      recent_scores = user.scores.where('created_at > ?', Time.now - 2.days)
-      if not recent_scores.empty?
-        user.update_attributes!({ :next_mailing_at => Time.now + rand(20..32).hours })
-      else
-        UserNotifier.delay.weekly_notification(user.id)
-        UserNotifier.delay.weekly_notification(user.id, true)
-        user.update_attributes!({ :next_mailing_at => Time.now + 7.days + rand(-12..12).hours })
-      end
+    # Reject if user solved a level in the last 2 days.
+    mail_users = mail_users.keep_if do |user|
+      user.scores.where('created_at > ?', Time.now - 2.days).empty?
     end
 
-    if repeat
-      MailingService.delayed_send_mailing
-    end
+    mail_users
   end
 end
 
