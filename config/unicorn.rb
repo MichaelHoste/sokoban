8d431022de518cac5e4360a427267b52feaf209b
Michaël Hoste <michael.hoste@gmail.com>
2013-05-16 18:41:25 +0200

    deploy

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,5 +1,5 @@
 working_directory "/home/deploy/apps/sokoban/current"
-#pid               "/home/deploy/apps/sokoban/shared/pids/unicorn.pid"
+pid               "/home/deploy/apps/sokoban/shared/pids/unicorn.pid"
 stderr_path       "/home/deploy/apps/sokoban/shared/log/unicorn.log"
 stdout_path       "/home/deploy/apps/sokoban/shared/log/unicorn.log"
 
