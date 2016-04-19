8b2901f9fe9f6c65ae3ea2894f554fe17ca00b4c
Michaël Hoste <michael.hoste@gmail.com>
2016-04-19 18:02:56 +0200

    debug fb

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -3,8 +3,7 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :facebook, ENV['FACEBOOK_APP_ID'], ENV['FACEBOOK_APP_SECRET'],
            :scope                  => ENV['FACEBOOK_SCOPE'],
-           :secure_image_url       => true,
-           :provider_ignores_state => true
+           :secure_image_url       => true
 end
 
 OmniAuth.config.on_failure = Proc.new { |env|
