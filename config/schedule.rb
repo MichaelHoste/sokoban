33a71b8855ec1e8c986c6d3256532e67fc799cd4
Michaël Hoste <michael.hoste@gmail.com>
2016-04-19 16:40:37 +0200

    debug schedule

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,6 +1,6 @@
-set      :output,        "#{path}/log/cron.log"
-job_type :bundle_exec,   'cd :path && bundle exec :task :output' # :path is set to project dir by default
-job_type :simple_command 'cd :path && :task :output'
+set      :output,         "#{path}/log/cron.log"
+job_type :bundle_exec,    'cd :path && bundle exec :task :output' # :path is set to project dir by default
+job_type :simple_command, 'cd :path && :task :output'
 
 every 1.day, :at => '4:30 am' do
   simple_command "backup perform --trigger sokoban -c #{path}/config/backup.rb"
