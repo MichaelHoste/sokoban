91d93be2219c30878e399b7eefce0622bdfb0b0d
Michaël Hoste <michael.hoste@gmail.com>
2016-12-30 11:54:42 +0100

    start delayed job on reboot

--- config/schedule.rb
+++ config/schedule.rb
@@ -2,6 +2,10 @@ set      :output,         "#{path}/log/cron.log"
 job_type :bundle_exec,    'cd :path && bundle exec :task :output' # :path is set to project dir by default
 job_type :simple_command, 'cd :path && :task :output'
 
+every :reboot do
+  command "cd #{path} && RAILS_ENV=production bin/delayed_job restart"
+end
+
 every 1.day, :at => '4:30 am' do
   simple_command "backup perform --trigger sokoban -c #{path}/config/backup.rb"
 end
