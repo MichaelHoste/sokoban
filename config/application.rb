f3a2a6e0b910dfb34f98a01c8a33d83a8c6d5eb6
Michaël Hoste <michael.hoste@gmail.com>
2021-09-02 15:16:23 +0200

    Update rails and dependencies from 5.2 to 6.0

--- config/application.rb
+++ config/application.rb
@@ -9,7 +9,7 @@ Bundler.require(*Rails.groups)
 module Sokojax
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 5.1
+    config.load_defaults 6.0
 
     config.time_zone           = 'UTC'
     config.i18n.default_locale = :en
