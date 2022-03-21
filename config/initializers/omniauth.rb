c7dbb722a6c1e32f4aeb5371ab71166a0ca34259
Michaël Hoste <michael.hoste@gmail.com>
2022-03-21 13:54:05 +0100

    Try to get friends when login

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -3,7 +3,7 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :facebook, ENV['FACEBOOK_APP_ID'], ENV['FACEBOOK_APP_SECRET'],
            :scope                  => ENV['FACEBOOK_SCOPE'],
-           :info_fields            => 'email,name,first_name,middle_name,last_name,picture',
+           :info_fields            => 'email,name,first_name,middle_name,last_name,picture,friends',
            :provider_ignores_state => true,
            :secure_image_url       => true
 end
